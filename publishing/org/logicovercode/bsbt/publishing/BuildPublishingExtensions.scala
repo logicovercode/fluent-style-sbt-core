68e9f377aff7afbb2b464f636d4eaa384262c40b
techLeadAtLogicOverCode <techlead@logicovercode.com>
2021-05-03 14:51:22 +0530

    traits saggregated

--- publishing/org/logicovercode/bsbt/publishing/BuildPublishingExtensions.scala
+++ publishing/org/logicovercode/bsbt/publishing/BuildPublishingExtensions.scala
@@ -1,27 +1,13 @@
 package org.logicovercode.bsbt.publishing
 
-import org.apache.ivy.core.module.descriptor.License
-import org.logicovercode.bsbt.build.{Build, BuildFactory}
 import org.logicovercode.bsbt.java_module.JavaBuild
 import org.logicovercode.bsbt.scala_module.ScalaBuild
 import sbt.Def
-import sbt.librarymanagement.{Developer, MavenRepository, ScmInfo}
+import sbt.librarymanagement.ScmInfo
 
 import java.net.URL
 
-trait BuildPublishingSettings[T <: Build[T]] extends BuildFactory[T] with PublishingSettings {
-  def argsRequiredForPublishing(
-                                 projectDevelopers: List[Developer],
-                                 license: License,
-                                 homePageUrl: URL,
-                                 moduleScmInfo: ScmInfo,
-                                 mavenRepository: MavenRepository
-                               ): T = {
 
-    val _settings = argsRequiredForPublishingSettings(projectDevelopers, license, homePageUrl, moduleScmInfo, mavenRepository)
-    moduleWithNewSettings( _settings)
-  }
-}
 
 trait BuildPublishingExtensions {
 
