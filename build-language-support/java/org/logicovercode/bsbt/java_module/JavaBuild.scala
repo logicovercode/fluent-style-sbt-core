635f11cd2fa63b805729de8972db143a085b1fcf
techLeadAtLogicOverCode <techlead@logicovercode.com>
2021-05-03 14:46:10 +0530

    added additional settings of java build

--- build-language-support/java/org/logicovercode/bsbt/java_module/JavaBuild.scala
+++ build-language-support/java/org/logicovercode/bsbt/java_module/JavaBuild.scala
@@ -1,14 +1,33 @@
 package org.logicovercode.bsbt.java_module
 
-import org.logicovercode.bsbt.build.{BuildApply, Build}
-import sbt.Def
+import org.logicovercode.bsbt.build.{Build, BuildApply}
+import sbt.Keys._
+import sbt._
 
 case class JavaBuild(override val sbtSettings: Set[Def.Setting[_]]) extends Build[JavaBuild](sbtSettings) {
 
+  def scalaLibInTestScope() : JavaBuild = {
+    val scalaLibInTestScopeSettings = Set(
+      libraryDependencies += "org.scala-lang" % "scala-library" % scalaVersion.value % Test
+    )
+    JavaBuild(sbtSettings ++ scalaLibInTestScopeSettings)
+  }
+
   override def moduleWithNewSettings(allSettings: Set[Def.Setting[_]]): JavaBuild = JavaBuild(allSettings)
 }
 
-object JavaBuild extends BuildApply[JavaBuild]{
+object JavaBuild extends BuildApply[JavaBuild] {
   override def moduleWithNewSettings(allSettings: Set[Def.Setting[_]]): JavaBuild = JavaBuild(allSettings)
-}
 
+  override def apply(projectOrganization: String, projectArtifact: String, mavenVersion: String): JavaBuild = {
+
+    val additionalJavaBuildSettings : Set[Def.Setting[_]] = Set(
+      publishMavenStyle := true,
+      crossPaths := false,
+      autoScalaLibrary := false
+    )
+
+    val javaBuild = super.apply(projectOrganization, projectArtifact, mavenVersion)
+    new JavaBuild(javaBuild.sbtSettings ++ additionalJavaBuildSettings)
+  }
+}
