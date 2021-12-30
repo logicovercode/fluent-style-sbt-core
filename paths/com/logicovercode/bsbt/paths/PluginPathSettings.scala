009f9a21266816de8edad0755b13b0ba8766eaac
techLeadAtLogicOverCode <techLead@logicovercode.com>
2021-12-30 13:30:13 +0530

    fix #7 : HOME variable should be a better File, not a string to create platform independent path objects

--- paths/com/logicovercode/bsbt/paths/PluginPathSettings.scala
+++ paths/com/logicovercode/bsbt/paths/PluginPathSettings.scala
@@ -1,11 +1,12 @@
 package com.logicovercode.bsbt.paths
 
+import better.files.File
 import com.logicovercode.wdocker.OsFunctions.isWindowsCategoryOs
 
 trait PluginPathSettings  {
 
   val HOME = isWindowsCategoryOs() match {
-    case true => sys.env("USERPROFILE")
-    case false    => sys.env("HOME")
+    case true => File( sys.env("USERPROFILE") )
+    case false    => File( sys.env("HOME") )
   }
 }
