b320e74a57619f0abf08f4c5ca16d8e2a9f45bcc
techLeadAtLogicOverCode <techLead@logicovercode.com>
2022-01-07 12:09:54 +0530

    default settings updated

--- build-core/com/logicovercode/bsbt/build/BuildInitialSettings.scala
+++ build-core/com/logicovercode/bsbt/build/BuildInitialSettings.scala
@@ -3,6 +3,8 @@ package com.logicovercode.bsbt.build
 import sbt._
 import sbt.Keys._
 
+import scala.util.Try
+
 
 object BuildInitialSettings {
 
@@ -12,15 +14,19 @@ object BuildInitialSettings {
              mavenVersion: String
            ): Set[Def.Setting[_]] = {
 
-    val sbtOfflineMode = sys.env.getOrElse("SBT_OFFLINE_MODE", "false").toBoolean
+    val sbtOfflineMode = Try(sys.env("SBT_OFFLINE_MODE").toBoolean).getOrElse(false)
 
     val defaultSettings: Set[Def.Setting[_]] = Set(
       name := projectArtifact,
       version := mavenVersion,
       organization := projectOrganization,
       offline := sbtOfflineMode,
-      publishConfiguration := publishConfiguration.value.withOverwrite(true),
-      publishLocalConfiguration := publishLocalConfiguration.value.withOverwrite(true)
+      publishLocalConfiguration := publishLocalConfiguration.value.withOverwrite(true),
+      //IMPORTANT : publishConfiguration is not set to true, because
+      //we don't want to overwrite artifact on sonatype, instead we want to get an error
+      //if republish attempted, when same version is already there on sonatype
+      //publishConfiguration := publishConfiguration.value.withOverwrite(true),
+      scalacOptions ++= Seq("-deprecation", "-feature")
     )
 
     defaultSettings
